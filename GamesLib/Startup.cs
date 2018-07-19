960ee1cbefb13ee0fc1a770252662cdb645f6677
Łukasz Maciąg <manix93@gmail.com>
2018-07-19 20:24:15 +0200

    Add project files.

---
+++ GamesLib/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GamesLib.Startup))]
+namespace GamesLib
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
