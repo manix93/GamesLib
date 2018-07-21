de51667d68b3cb1c8027e0ac69b5a02a7178015d
Łukasz Maciąg <manix93@gmail.com>
2018-07-21 11:32:00 +0200

    Add API to delete Games

---
+++ GamesLib/Controllers/API/GamesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Web.Configuration;
+using System.Web.Http;
+using GamesLib.Models;
+
+namespace GamesLib.Controllers.API
+{
+    public class GamesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public GamesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteGame(int id)
+        {
+            var game = _context.Games.SingleOrDefault(g => g.Id.Equals(id));
+
+            if (game.Equals(null))
+                return NotFound();
+
+            _context.Games.Remove(game);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
